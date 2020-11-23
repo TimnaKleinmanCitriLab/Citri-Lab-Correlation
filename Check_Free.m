b853603d441caf135c727874308c37c520da818b
TimnaKleinman <timna.kleinman@mail.huji.ac.il>
2020-11-23 20:39:50 +0200

    Added updating free

--- Check_Free.m
+++ Check_Free.m
@@ -5,7 +5,7 @@ SMOOTH_FACTOR = 300;
 
 
 FOLDER = "\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\";
-IN_FOLDER = "\free\Free_comb_old.mat";
+IN_FOLDER = "\free\Free_comb.mat";
 
 
 freeMouse = load(FOLDER + MOUSE_NAME + IN_FOLDER);
@@ -45,3 +45,114 @@ for rowIndex = 1:size(freeMouse.t_info, 1)
     ylabel("zscored \DeltaF/F", 'FontSize', 14)
     xlim([0 100])
 end
+
+
+%% Load Free
+% load('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\3_from410\free\Free_comb_original.mat')
+% t_info.organized_date = [2002041635; 2002041716];
+% t_info = movevars(t_info, 'organized_date', 'Before', 'date');
+% t_info.pre_or_post = ["post"; "post"];
+% t_info = movevars(t_info, 'pre_or_post', 'Before', 'fs');
+% t_info.hemisphere = ['l'; 'r'];
+% t_info.strength = ['700/300'; '???/???'];
+% t_info.recording_notes = {''; 'a bit more signal in geco'};
+% t_info.other = {''; ''};
+% t_info.display = [0; 0];
+% t_info.reason_not = [""; ""];
+% save('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\3_from410\free\Free_comb.mat', 't_info', 'af_trials', 'all_trials')
+% 
+% 
+% load('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\4_from410L\free\Free_comb_original.mat')
+% t_info.organized_date = [2001291254];
+% t_info = movevars(t_info, 'organized_date', 'Before', 'date');
+% t_info.pre_or_post = ["post"];
+% t_info = movevars(t_info, 'pre_or_post', 'Before', 'fs');
+% t_info.hemisphere = ['l'];
+% t_info.strength = ['600/250'];
+% t_info.recording_notes = {'Geco flat + 40 min recording'};
+% t_info.other = {''};
+% t_info.display = [0];
+% t_info.reason_not = [""];
+% save('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\4_from410L\free\Free_comb.mat', 't_info', 'af_trials', 'all_trials')
+% 
+% 
+% load('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\3_from430\free\Free_comb_original.mat')
+% t_info.organized_date = [2005241328; 2005241352];
+% t_info = movevars(t_info, 'organized_date', 'Before', 'date');
+% t_info.pre_or_post = ["post"; "post"];
+% t_info = movevars(t_info, 'pre_or_post', 'Before', 'fs');
+% t_info.hemisphere = ['l'; 'r'];
+% t_info.strength = {'1000/400'; '600/300'};
+% t_info.recording_notes = {'Kind of good + 20 min'; 'Stick with r hemi + 20 min'};
+% t_info.other = {''; ''};
+% t_info.display = [0; 0];
+% t_info.reason_not = [""; ""];
+% save('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\3_from430\free\Free_comb.mat', 't_info', 'af_trials', 'all_trials')
+% 
+% 
+% load('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\4_from430\free\Free_comb_original.mat')
+% t_info.organized_date = [2005241328; 2005241352];
+% t_info = movevars(t_info, 'organized_date', 'Before', 'date');
+% t_info.pre_or_post = ["post"; "post"];
+% t_info = movevars(t_info, 'pre_or_post', 'Before', 'fs');
+% t_info.hemisphere = ['l'; 'r'];
+% t_info.strength = {'600/300'; '600/250'};
+% t_info.recording_notes = {''; 'Bad geco for some reason'};
+% t_info.other = {''; ''};
+% t_info.display = [0; 0];
+% t_info.reason_not = [""; ""];
+% save('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\4_from430\free\Free_comb.mat', 't_info', 'af_trials', 'all_trials')
+
+
+load('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\1_from500\free\Free_comb_original.mat')
+t_info.organized_date = [2006161224; 2006161315];
+t_info = movevars(t_info, 'organized_date', 'Before', 'date');
+t_info.pre_or_post = ["post"; "post"];
+t_info = movevars(t_info, 'pre_or_post', 'Before', 'fs');
+t_info.hemisphere = ['r'; 'l'];
+t_info.strength = ['1000/200'; '1000/200'];
+t_info.recording_notes = {'good gcamp, geco - meh + 40 min'; ''};
+t_info.other = {''; ''};
+t_info.display = [0; 0];
+t_info.reason_not = [""; ""];
+save('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\1_from500\free\Free_comb.mat', 't_info', 'af_trials', 'all_trials')
+
+load('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\2_from500\free\Free_comb_original.mat')
+t_info.organized_date = [2006231613; 2006231659; 2006291746];
+t_info = movevars(t_info, 'organized_date', 'Before', 'date');
+t_info.pre_or_post = ["post"; "post"; "post"];
+t_info = movevars(t_info, 'pre_or_post', 'Before', 'fs');
+t_info.hemisphere = ['l'; 'r'; '?'];
+t_info.strength = {'600/200'; '600/200'; '?'};
+t_info.recording_notes = {''; ''; ''};
+t_info.other = {''; ''; ''};
+t_info.display = [0; 0; 0];
+t_info.reason_not = [""; ""; ""];
+save('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\2_from500\free\Free_comb.mat', 't_info', 'af_trials', 'all_trials')
+
+load('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\3_from500\free\Free_comb_original.mat')
+t_info.organized_date = [2006231748; 2006231839; 2006291757];
+t_info = movevars(t_info, 'organized_date', 'Before', 'date');
+t_info.pre_or_post = ["post"; "post"; "post"];
+t_info = movevars(t_info, 'pre_or_post', 'Before', 'fs');
+t_info.hemisphere = ['r'; 'l'; '?'];
+t_info.strength = {'300/50'; '500/300'; '?'};
+t_info.recording_notes = {'Both channels have signal'; ''; ''};
+t_info.other = {''; ''; ''};
+t_info.display = [0; 0; 0];
+t_info.reason_not = [""; ""; ""];
+save('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\3_from500\free\Free_comb.mat', 't_info', 'af_trials', 'all_trials')
+
+
+% load('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\4_from440\free\Free_comb_original.mat')
+% t_info.organized_date = [2007191818; 2007191903];
+% t_info = movevars(t_info, 'organized_date', 'Before', 'date');
+% t_info.pre_or_post = ["post"; "post"];
+% t_info = movevars(t_info, 'pre_or_post', 'Before', 'fs');
+% t_info.hemisphere = ['l'; 'r'];
+% t_info.strength = {'800/500'; '800/250'};
+% t_info.recording_notes = {'Signal in both, but has artifacts + r hemi was better'; 'Less gcamp, but less artifacts + r hemi was better'};
+% t_info.other = {''; ''};
+% t_info.display = [0; 0];
+% t_info.reason_not = [""; ""];
+% save('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\4_from440\free\Free_comb.mat', 't_info', 'af_trials', 'all_trials')
