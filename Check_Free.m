1f6ab5c1ff1246de53e9654d27d11df0bac8c827
TimnaKleinman <timna.kleinman@mail.huji.ac.il>
2020-12-07 13:05:37 +0200

    General Updates

--- Check_Free.m
+++ Check_Free.m
@@ -1,53 +1,3 @@
-% Was useful for uploading free, has no use either then that
-checkMouse()
-
-function checkMouse()
-    MOUSE_NAME = "4_from440";
-    SMOOTH_FACTOR = 300;
-
-    FOLDER = "\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\";
-    IN_FOLDER = "\free\Free_comb_old.mat";
-
-
-    freeMouse = load(FOLDER + MOUSE_NAME + IN_FOLDER);
-
-    for rowIndex = 1:size(freeMouse.t_info, 1)
-        gcampSignal = freeMouse.all_trials(rowIndex);
-        gcampSignal = gcampSignal{:};
-        jrgecoSignal = freeMouse.af_trials(rowIndex);
-        jrgecoSignal = jrgecoSignal{:};
-
-        signalLen = size(gcampSignal, 2);
-        fs = freeMouse.t_info.fs(rowIndex);
-        trialTime = signalLen / fs;
-        % timeVec1 = 0:1/fs:trialTime;
-        timeVector = linspace(0, trialTime, signalLen);
-
-        gcampSignal = smooth(gcampSignal', SMOOTH_FACTOR)';
-        jrgecoSignal = smooth(jrgecoSignal', SMOOTH_FACTOR)';
-
-        signalCorrelation = corr(gcampSignal', jrgecoSignal');
-
-        figure("Name", "Signal from all sessions of mouse", "NumberTitle", "off");
-        ax = gca;
-
-        plot(ax, timeVector, gcampSignal, 'LineWidth', 1.5, 'Color', '#009999');
-        hold on;
-        plot(ax, timeVector, jrgecoSignal, 'LineWidth', 1.5, 'Color', '#990099');
-        hold off;
-        title({"Mouse: " + MOUSE_NAME, " Row:" + rowIndex, "Smooth factor: " + SMOOTH_FACTOR, "Correlation: " + signalCorrelation}, 'interpreter', 'none')
-
-        legend("gcamp", "jrgeco")
-
-%         [gcampType, jrgecoType] = mouse1.findGcampJrgecoType();
-
-%         legend(gcampType + " (gcamp)", jrgecoType + " (jrgeco)", 'Location', 'best', 'Interpreter', 'none')
-        xlabel("Time (sec)", 'FontSize', 14)
-        ylabel("zscored \DeltaF/F", 'FontSize', 14)
-        xlim([0 100])
-    end
-end
-
 %% Save Free
 save('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\####\free\Free_comb.mat', 't_info', 'af_trials', 'all_trials')
 
@@ -160,3 +110,55 @@ save('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\####\free\Free_comb.mat', 
 % t_info.display = [0; 0];
 % t_info.reason_not = [""; ""];
 % save('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\4_from440\free\Free_comb.mat', 't_info', 'af_trials', 'all_trials')
+
+
+%% Was useful for uploading free, has no use either then that
+checkMouse()
+
+function checkMouse()
+    MOUSE_NAME = "2_from500";
+    SMOOTH_FACTOR = 300;
+
+    FOLDER = "\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\";
+    IN_FOLDER = "\free\Free_comb.mat";
+
+
+    freeMouse = load(FOLDER + MOUSE_NAME + IN_FOLDER);
+
+    for rowIndex = 1:size(freeMouse.t_info, 1)
+        gcampSignal = freeMouse.all_trials(rowIndex);
+        gcampSignal = gcampSignal{:};
+        jrgecoSignal = freeMouse.af_trials(rowIndex);
+        jrgecoSignal = jrgecoSignal{:};
+
+        signalLen = size(gcampSignal, 2);
+        fs = freeMouse.t_info.fs(rowIndex);
+        trialTime = signalLen / fs;
+        % timeVec1 = 0:1/fs:trialTime;
+        timeVector = linspace(0, trialTime, signalLen);
+
+        gcampSignal = smooth(gcampSignal', SMOOTH_FACTOR)';
+        jrgecoSignal = smooth(jrgecoSignal', SMOOTH_FACTOR)';
+
+        signalCorrelation = corr(gcampSignal', jrgecoSignal');
+
+        figure("Name", "Signal from all sessions of mouse", "NumberTitle", "off");
+        ax = gca;
+
+        plot(ax, timeVector, gcampSignal, 'LineWidth', 1.5, 'Color', '#009999');
+        hold on;
+        plot(ax, timeVector, jrgecoSignal, 'LineWidth', 1.5, 'Color', '#990099');
+        hold off;
+        title({"Mouse: " + MOUSE_NAME, " Row:" + rowIndex, "Smooth factor: " + SMOOTH_FACTOR, "Correlation: " + signalCorrelation}, 'interpreter', 'none')
+
+        legend("gcamp", "jrgeco")
+
+%         [gcampType, jrgecoType] = mouse1.findGcampJrgecoType();
+
+%         legend(gcampType + " (gcamp)", jrgecoType + " (jrgeco)", 'Location', 'best', 'Interpreter', 'none')
+        xlabel("Time (sec)", 'FontSize', 14)
+        ylabel("zscored \DeltaF/F", 'FontSize', 14)
+        xlim([0 100])
+    end
+end
+
---
+++ OtherHelpers.m
@@ -0,0 +1,117 @@
+listOfLists = [AccInAccOut, AudAcc, AudInAccOut, AudInAudOut, OfcAcc];
+for mouseList = listOfLists
+%     mouseList.plotCrossAndAutoCorrealtionByMouse(["Free", "post"], 0, 300, 10, true)
+%     mouseList.plotCorrelationBar(300, 10)
+%     mouseList.plotSlidingCorrelationBar(3, 0.2, 300, 10)
+%     mouseList.plotCrossCorrelationLagBar(["Free", "post"], 1.5, 300, 10, true)
+%     mouseList.plotCrossCorrelationLagBar(["Passive", "awake","FS", "post"], 1.5, 300, 10, true)
+    mouseList.plotCrossCorrelationLagBar(["Passive", "awake","BBN", "post"], 1.5, 300, 10, true)
+end
+
+
+
+for mouse = AccInAccOut.LoadedMouseList
+    mouse.plotSlidingCorrelationAll(["Task", "onset"], 3, 0.2, 300, 100)
+end
+
+AccInAccOut.plotCrossCorrelationLagBar(["Task", "onset"], 5, 300, 10, false)
+AudAcc.plotCrossCorrelationLagBar(["Task", "onset"], 5, 300, 10, false)
+AudInAudOut.plotCrossCorrelationLagBar(["Task", "onset"], 5, 300, 10, false)
+AudInAccOut.plotCrossCorrelationLagBar(["Task", "onset"], 5, 300, 10, false)
+OfcAcc.plotCrossCorrelationLagBar(["Task", "onset"], 5, 300, 10, false)
+
+
+listOfLists = [AccInAccOut, AudAcc, AudInAccOut, AudInAudOut, OfcAcc];
+
+times = [];
+names = [];
+for mouseList = listOfLists
+    for mouse = mouseList.LoadedMouseList
+            [~, ~, ~, totalTime, ~] = mouse.getInformationReshapeDownsampleAndSmooth(["Passive", "awake", "FS", "post"], 300, 10);
+            times = [times, totalTime];
+            names = [names, mouse.Name];
+    end
+end
+
+
+% Task
+for mouse = AccInAccOut.LoadedMouseList
+mouse.plotSlidingCorrelationAll(["Task", "onset"], 3, 0.2, 300, 100)
+end
+for mouse = AudAcc.LoadedMouseList
+mouse.plotSlidingCorrelationAll(["Task", "onset"], 3, 0.2, 300, 100)
+end
+for mouse = AudInAccOut.LoadedMouseList
+mouse.plotSlidingCorrelationAll(["Task", "onset"], 3, 0.2, 300, 100)
+end
+for mouse = AudInAudOut.LoadedMouseList
+mouse.plotSlidingCorrelationAll(["Task", "onset"], 3, 0.2, 300, 100)
+end
+for mouse = OfcAcc.LoadedMouseList
+mouse.plotSlidingCorrelationAll(["Task", "onset"], 3, 0.2, 300, 100)
+end
+
+% Free - pre
+for mouse = OfcAcc.LoadedMouseList
+if ~(mouse.Name == "2 from 430")
+mouse.plotSlidingCorrelationAll(["Free", "pre"], 3, 0.2, 300, 100)
+end
+end
+
+% Free - post
+for mouse = AccInAccOut.LoadedMouseList
+mouse.plotSlidingCorrelationAll(["Free", "post"], 3, 0.2, 300, 100)
+end
+for mouse = AudAcc.LoadedMouseList
+if ~(mouse.Name == "4 from 410")
+mouse.plotSlidingCorrelationAll(["Free", "post"], 3, 0.2, 300, 100)
+end
+end
+for mouse = AudInAccOut.LoadedMouseList
+mouse.plotSlidingCorrelationAll(["Free", "post"], 3, 0.2, 300, 100)
+end
+for mouse = AudInAudOut.LoadedMouseList
+mouse.plotSlidingCorrelationAll(["Free", "post"], 3, 0.2, 300, 100)
+end
+for mouse = OfcAcc.LoadedMouseList
+mouse.plotSlidingCorrelationAll(["Free", "post"], 3, 0.2, 300, 100)
+end
+
+
+
+% Load Signals
+gcamp = handles.synapse.TRACE.streams.x65F.data;
+jrgeco = handles.synapse.TRACE.streams.x60J.data;
+load('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\2_from500\free\Free_comb.mat')                                       % CHANGE!
+gcamp = {gcamp};
+jrgeco = {jrgeco};
+all_trials(4) = gcamp;                                                                                                     % CHANGE!
+af_trials(4) = jrgeco;                                                                                                     % CHANGE!
+added_data = {2011301927, "30/11/20", "19:27:17", "post", handles.synapse.TRACE.streams.x60J.fs, "r", '?', '', '', 1, ""}; % CHANGE!
+t_info =  [t_info; added_data];
+
+save('\\132.64.59.21\Citri_Lab\gala\Phys data\New Rig\2_from500\free\Free_comb.mat', 't_info', 'af_trials', 'all_trials')  % CHANGE!
+
+
+% Plot signals
+gcamp = handles.synapse.TRACE.streams.x65F.data;
+jrgeco = handles.synapse.TRACE.streams.x60J.data;
+
+fs = handles.synapse.TRACE.streams.x60J.fs;
+totalTime = round(size(gcamp, 2) / fs);
+timeVector = linspace(0, totalTime, size(gcamp, 2));
+
+figure("Name", "Signal from all sessions of mouse ", "NumberTitle", "off");
+ax = gca;
+
+plot(ax, timeVector, gcamp, 'LineWidth', 1.5, 'Color', '#009999');
+hold on;
+plot(ax, timeVector, jrgeco, 'LineWidth', 1.5, 'Color', '#990099');
+hold off;
+
+title(ax, "Signal From: 2-430 0710 1027", 'FontSize', 12)
+
+legend( "gcamp", "jrgeco", 'Location', 'best', 'Interpreter', 'none')
+xlabel("Time (sec)", 'FontSize', 14)
+ylabel("zscored \DeltaF/F", 'FontSize', 14)
+xlim([0 100])
